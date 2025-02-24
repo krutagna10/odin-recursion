86c784f94cbb7eea306026b4c8597a249e525713
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-24 16:58:51 +0530

    add fibonacci-sequence-recursive

--- fibonacci-sequence/fibonacci-sequence-recursive.ts
+++ fibonacci-sequence/fibonacci-sequence-recursive.ts
@@ -1,3 +1,9 @@
-function getFibonacciSequence() {
-  
+function getFibonacciSequence(num: number, sequence: number[] = [0, 1]) {
+  if (sequence.length >= num) {
+    return sequence.slice(0, num);
+  }
+  const fibonacciElement = sequence.at(-1) + sequence.at(-2);
+  sequence.push(fibonacciElement);
+  return getFibonacciSequence(num, sequence);
 }
+
